b9e513758218dd85379d37a39e5ffe91715732fe
Konstantin <aziimov1999123@gmail.com>
2023-12-14 02:48:15 +0300

    final commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,14 @@
 import "./styles.css";
-
 import Header from "./components/header/header.js";
-
 import Main from "./components/main/Main.js";
-
 import Footer from "./components/footer/footer.js";
-
 import Skills from "./pages/Skills.js";
-
 import Contacts from "./pages/Contacts.js";
-
 import Projects from "./pages/Projects.js";
-
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-
-import Project from "./components/main/project/project.js";
-
+import Project from "./pages/Project";
 import ScrollToTop from "./utils/scrollToTop.js" ;
+
 
 function App() {
   return (
@@ -27,7 +19,7 @@ function App() {
      <Routes>
       <Route path="/" element={<Main/>} />
       <Route path="/projects" element={<Projects/>} />
-      <Route path="/project" element={<Project/>} />
+      <Route path="/project/:id" element={<Project/>} />
       <Route path="/contacts" element={<Contacts/>}/>
       <Route path="/Skills" element={<Skills/>} />
      </Routes>
